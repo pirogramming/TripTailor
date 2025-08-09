5f53684ddf4e548541392a5415a88bd0ec884422
Jinecastle03 <gptjd0717@korea.ac.kr>
2025-08-09 20:26:12 +0900

    fix: url.py, base.html 수정

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,6 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.login_page, name='login_page'),
-    path('', main_page, name='main_page'),  # ✅ /
-    path('main/', main_page, name='main_page'),
+    path('', views.main_page, name='main_page'),  # ✅ /
+    path('main/', views.main_page, name='main_page'),
 ]
