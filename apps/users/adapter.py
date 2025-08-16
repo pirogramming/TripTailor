e2ea3c215d40992708ef5a9d2dbe90b1d8ef52f3
ParkJueun <jueun036@gmail.com>
2025-08-16 16:44:36 +0900

    fix: 대분류 선택시 지도 버그 해결

--- apps/users/adapter.py
+++ apps/users/adapter.py
@@ -6,3 +6,14 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
         # 이미 로그인된 유저라면 바로 redirect
         if request.user.is_authenticated:
             return redirect('/')
+    
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+
+        # 소셜 로그인 유저라면 provider, uid 설정
+        if sociallogin.account:
+            user.provider = sociallogin.account.provider  # "google", "kakao", "naver"
+            user.provider_uid = sociallogin.account.uid
+            user.save()
+
+        return user
