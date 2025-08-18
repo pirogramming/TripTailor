03566c670abbf1285f3fed6a02bafc4bf79fc40a
ParkJueun <jueun036@gmail.com>
2025-08-18 22:52:05 +0900

    fix:로그인 수정

--- apps/users/adapter.py
+++ apps/users/adapter.py
@@ -5,6 +5,7 @@ from django.utils.text import slugify
 from django.contrib.auth import get_user_model
 from allauth.exceptions import ImmediateHttpResponse
 from django.contrib import messages
+from allauth.socialaccount.models import SocialAccount
 
 User = get_user_model()
 
@@ -14,17 +15,19 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
             return redirect('/')
 
         email = sociallogin.account.extra_data.get("email")
+        provider = sociallogin.account.provider  # ex: 'google', 'kakao', 'naver'
+
         if email:
             try:
                 existing_user = User.objects.get(email=email)
 
-                # ✅ 해당 이메일이 '소셜 유저'가 아닌 경우만 로그인 막기
-                if not hasattr(existing_user, "socialaccount"):
+                # ✅ 이 이메일의 소셜 계정이 현재 provider인지 확인
+                if not SocialAccount.objects.filter(user=existing_user, provider=provider).exists():
                     messages.error(request, "이미 해당 이메일로 가입된 계정이 있습니다. 소셜 로그인할 수 없습니다.")
                     raise ImmediateHttpResponse(redirect("/accounts/login/"))
 
             except User.DoesNotExist:
-                pass  # 존재하지 않으면 그냥 자동가입 진행
+                pass  # 없으면 자동가입 진행
 
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
