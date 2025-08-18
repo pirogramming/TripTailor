2da38cb34b0b92d45dc5ab4af26b9f1c5814a880
ParkJueun <jueun036@gmail.com>
2025-08-18 21:51:57 +0900

    test

--- apps/users/adapter.py
+++ apps/users/adapter.py
@@ -1,5 +1,7 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.shortcuts import redirect
+from allauth.account.utils import user_username
+from django.utils.text import slugify
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
@@ -9,11 +11,14 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
+        nickname = sociallogin.account.extra_data.get("name", "")
+        if nickname:
+            user_username(user, slugify(nickname))
 
         # 소셜 로그인 유저라면 provider, uid 설정
         if sociallogin.account:
             user.provider = sociallogin.account.provider  # "google", "kakao", "naver"
             user.provider_uid = sociallogin.account.uid
-            user.save()
-
+            
+        user.save()
         return user
