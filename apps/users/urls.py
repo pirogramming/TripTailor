c8cc14fe5f4dfd29c273136f510b0a091f014dd8
ParkJueun <jueun036@gmail.com>
2025-08-18 19:52:55 +0900

    test

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -21,11 +21,12 @@ urlpatterns = [
         template_name='account/password_reset_done.html'
     ), name='password_reset_done'),
 
-    path('password/reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='users/password_reset_confirm.html'
-    ), name='password_reset_confirm'),
+    path('password/reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+        template_name="admin/password_reset_confirm.html",
+        name='password_reset_confirm',
+    ),
 
     path('password/reset/complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='users/password_reset_complete.html'
+        template_name='admin/password_reset_complete.html'
     ), name='password_reset_complete'),
 ]
